5fc5bff109652fd4de71f6a7643cb8dd33b96635
Matthew <matthewhunter404@gmail.com>
2016-08-31 22:25:53 +0300

    Adds an onclick event handler for the listview to make a toast popup displaying the apps name.

--- app/src/main/java/matt/portfolio/MainActivity.java
+++ app/src/main/java/matt/portfolio/MainActivity.java
@@ -1,5 +1,7 @@
 package matt.portfolio;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public ArrayAdapter mListAdapter; //Due to the simplicity of this app, a default ArrayAdapter will do fine for now.
@@ -18,18 +22,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); //Sets up the default toolbar at the top of the app.
         setSupportActionBar(toolbar);
 
         //Todo: Replace dummy data
-        String Dummydata[]={"Popular Movies","Stock Hawk","Build it Bigger","Make Your App Material","Go Ubiquitous","Capstone"}; //this dummy data will be in place for testing
+        final String Dummydata[]={"Popular Movies","Stock Hawk","Build it Bigger","Make Your App Material","Go Ubiquitous","Capstone"}; //this dummy data will be in place for testing
         mListView = (ListView) findViewById(R.id.main_listview);    //assigns the main listview in the layout to mListView
         mListAdapter =                  //Defines the ArrayAdapter using:
                 new ArrayAdapter(this, // The current context (this activity)
                         R.layout.list_item, // The name of the layout ID.
                         R.id.textdisplay, // The ID of the textview to populate in each layout.
                         Dummydata);
-        mListView.setAdapter(mListAdapter); //Sets the adapter for the main listview to the array adapter defiend above
+        mListView.setAdapter(mListAdapter); //Sets the adapter for the main listview to the array adapter defined above
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> l, View view, int position, long id) {
+                // This code was derived primarily from the example provided by Google at https://developer.android.com/guide/topics/ui/notifiers/toasts.html
+                Context context = getApplicationContext();
+                CharSequence text = Dummydata[position];
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+
+        });
     }
 
 //    @Override
