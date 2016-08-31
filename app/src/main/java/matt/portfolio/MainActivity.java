3f710f8deb7a032678320422e53a0d82b7095557
Matthew <matthewhunter404@gmail.com>
2016-08-31 22:17:36 +0300

    Adds dummy data and sets up the arrayadapter to display this data to the listview on App startup. Also gives the main listview a proper ID.

--- app/src/main/java/matt/portfolio/MainActivity.java
+++ app/src/main/java/matt/portfolio/MainActivity.java
@@ -8,9 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public ArrayAdapter mListAdapter; //Due to the simplicity of this app, a default ArrayAdapter will do fine for now.
+    private ListView mListView;        //This ListView will have the main of the app listview assigned to it.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Todo: Replace dummy data
+        String Dummydata[]={"Popular Movies","Stock Hawk","Build it Bigger","Make Your App Material","Go Ubiquitous","Capstone"}; //this dummy data will be in place for testing
+        mListView = (ListView) findViewById(R.id.main_listview);    //assigns the main listview in the layout to mListView
+        mListAdapter =                  //Defines the ArrayAdapter using:
+                new ArrayAdapter(this, // The current context (this activity)
+                        R.layout.list_item, // The name of the layout ID.
+                        R.id.textdisplay, // The ID of the textview to populate in each layout.
+                        Dummydata);
+        mListView.setAdapter(mListAdapter); //Sets the adapter for the main listview to the array adapter defiend above
     }
 
 //    @Override
